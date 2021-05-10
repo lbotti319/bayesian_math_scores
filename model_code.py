3b1fe87959ed6d55457fc17ac3fc91da4b99414d
Ines P <ip221@georgetown.edu>
2021-05-09 22:44:06 -0400

    updates

--- model_code.py
+++ model_code.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
 from scipy.stats import multivariate_normal as mvnorm
-from scipy.stats import uniform, invgamma
-
+from scipy.stats import uniform, invgamma, bernoulli, poisson, norm
+import statsmodels.api as sm
 
 def log_beta_prob(X, y, beta, var=1):
     """
@@ -92,4 +91,136 @@ def Gibbs_regression(X, y, B):
     return betas[:,B:], accept[B:]
 
 
+###################################################################
+######### functions for missing data part of project ##############
+###################################################################
+
+
+def MH_step(param, target, proposal, accepts_param, *args):
+    accepted = 0
+    param_star = proposal.rvs()
+    u = uniform.rvs()
+    r = target(param_star, *args) / target(param, *args)
+    if u < r:
+        param = param_star
+        accepts_param += 1
+    return param, accepts_param
+
+
+def dist_alpha0(alpha0, alpha1, higher_yes, age_missing_higher_yes):
+    p = np.exp(alpha0 + alpha1*age_missing_higher_yes) / (1 + np.exp(alpha0 + alpha1*age_missing_higher_yes))
+    return np.prod(bernoulli.pmf(higher_yes, p)) * norm.pdf(alpha0, loc=0, scale=100)
+
+
+def dist_alpha1(alpha1, alpha0, higher_yes, age_missing_higher_yes):
+    p = np.exp(alpha0 + alpha1*age_missing_higher_yes) / (1 + np.exp(alpha0 + alpha1*age_missing_higher_yes))
+    return np.prod(bernoulli.pmf(higher_yes, p)) * norm.pdf(alpha1, loc=0, scale=100)
+
+
+def dist_gamma0(gamma0, gamma1, absences, age_missing_absences):
+    mu = np.exp(gamma0 + gamma1*age_missing_absences)
+    return np.prod(poisson.pmf(absences, mu)) * norm.pdf(gamma0, loc=0, scale=100)
+
+
+def dist_gamma1(gamma1, gamma0, absences, age_missing_absences):
+    mu = np.exp(gamma0 + gamma1*age_missing_absences)
+    return np.prod(poisson.pmf(absences, mu)) * norm.pdf(gamma1, loc=0, scale=100)
+
+
+def Gibbs_MH(X, y, B, n, higher_yes_col, absences_col, tau):
+    """
+
+    """
+    ################# Initializations ################
+    # regression parameters
+    betas = np.zeros((X.shape[1], 2 * B))
+    sigmas2 = np.zeros(2 * B)
+
+    # parameters for missing covariates
+    higher_yes_missing_idx = np.where(np.isnan(X[:, higher_yes_col]))[0]
+    higher_yes_sim = np.zeros((len(higher_yes_missing_idx), 2 * B))
+    alphas0, alphas1 = np.zeros(2 * B), np.zeros(2 * B)
+
+    absences_missing_idx = np.where(np.isnan(X[:, absences_col]))[0]
+    absences_sim = np.zeros((len(absences_missing_idx), 2 * B))
+    gammas0, gammas1 = np.zeros(2 * B), np.zeros(2 * B)
+
+    # getting ages for the missing values
+    age_missing_higher_yes = X[higher_yes_missing_idx, 1]
+    age_missing_absences = X[absences_missing_idx, 1]
+
+    # Initialize parameters
+    higher_yes = round(np.nanmean(X[:, higher_yes_col]))
+    higher_yes_sim[:, 0] = higher_yes
+    X[higher_yes_missing_idx, higher_yes_col] = higher_yes_sim[:, 0]
+    alpha0, alpha1 = 0.5, 0.5
+    alphas0[0], alphas1[0] = alpha0, alpha1
+
+    absences = round(np.nanmean(X[:, absences_col]))
+    absences_sim[:, 0] = absences
+    X[absences_missing_idx, absences_col] = absences_sim[:, 0]
+    gamma0, gamma1 = 0.05, 0.05
+    gammas0[0], gammas1[0] = gamma0, gamma1
+
+    reg = sm.OLS(y, X)
+    res = reg.fit()
+    beta_hat = res.params
+    # same as:
+    # np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+    sigma2 = res.mse_total
+    vbeta = np.linalg.inv(X.T.dot(X))
+    betas[:, 0] = beta_hat.copy()
+    sigmas2[0] = sigma2
+
+    accepts_alpha0, accepts_alpha1, accepts_gamma0, accepts_gamma1 = 0, 0, 0, 0
+    ##################################################
+
+    ############ Sampling Gibbs + MH #################
+    proposal_alpha0 = norm(loc=0, scale=tau)
+    proposal_alpha1 = norm(loc=0, scale=tau)
+    proposal_gamma0 = norm(loc=0, scale=tau)
+    proposal_gamma1 = norm(loc=0, scale=tau)
+
+    for i in range(1, 2 * B):
+        # sample a beta
+        beta = mvnorm(mean=beta_hat, cov=sigma2 * vbeta, allow_singular=True).rvs()
+        # sample a sigma2
+        sigma2 = invgamma.rvs(n / 2, (y - X.dot(beta)).T.dot(y - X.dot(beta)) / 2)
+        # sample the missing higher_yes
+        p = np.exp(alpha0 + alpha1 * age_missing_higher_yes) / (1 + np.exp(alpha0 + alpha1 * age_missing_higher_yes))
+        higher_yes = bernoulli.rvs(p)
+        # sample the missing absences
+        mu = np.exp(gamma0 + gamma1 * age_missing_absences)
+        absences = poisson.rvs(mu)
+        # sample alpha0, alpha1
+        alpha0, accepts_alpha0 = MH_step(alpha0, dist_alpha0, proposal_alpha0, accepts_alpha0,
+                                         alpha1, higher_yes, age_missing_higher_yes)
+        alpha1, accepts_alpha1 = MH_step(alpha1, dist_alpha1, proposal_alpha1, accepts_alpha1,
+                                         alpha0, higher_yes, age_missing_higher_yes)
+        # sample gamma0, gamma1
+        gamma0, accepts_alpha0 = MH_step(gamma0, dist_gamma0, proposal_gamma0, accepts_gamma0,
+                                         gamma1, absences, age_missing_absences)
+        gamma1, accepts_alpha1 = MH_step(gamma1, dist_gamma1, proposal_gamma1, accepts_gamma1,
+                                         gamma0, absences, age_missing_absences)
+
+        # updates
+        betas[:, i] = beta
+        sigmas2[i] = sigma2
+        higher_yes_sim[:, i] = higher_yes
+        absences_sim[:, i] = absences
+        alphas0[i] = alpha0
+        alphas1[i] = alpha1
+        gammas0[i] = gamma0
+        gammas1[i] = gamma1
+        X[higher_yes_missing_idx, higher_yes_col] = higher_yes
+        X[absences_missing_idx, absences_col] = absences
+        beta_hat = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+        vbeta = np.linalg.inv(X.T.dot(X))
+    ##################################################
+
+    return (betas, sigmas2, higher_yes_sim, absences_sim, alphas0, alphas1, gammas0, gammas1,
+            accepts_alpha0, accepts_alpha1, accepts_gamma0, accepts_gamma0)
+
+###################################################################
+###################################################################
 
